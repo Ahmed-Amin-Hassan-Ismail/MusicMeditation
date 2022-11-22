86164a8f3b9ac511a3bc1bb2b2ba53866887b6c5
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-11-22 20:14:23 +0200

    add playerView and slider to indicate for music time

--- MusicMeditation/MusicMeditation/Views/PlayerView.swift
+++ MusicMeditation/MusicMeditation/Views/PlayerView.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct PlayerView: View {
+    @State private var value: Double = 0.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("image-stones")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.width)
+                .ignoresSafeArea(.all)
+            
+            VStack(spacing: 32) {
+                closeButton
+                descriptionTitle
+                Spacer()
+                sliderWithTime
+            }
+            .padding(20)
+            
+        }
+       
+    }
+}
+
+extension PlayerView {
+    //MARK: - Close Button
+    private var closeButton: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Image(systemName: "xmark.circle.fill")
+                    .font(.system(size: 36))
+                    .foregroundColor(.white)
+                
+            }
+            Spacer()
+        }
+    }
+    
+    //MARK: - Munite of relaxing
+    private var descriptionTitle: some View {
+        Text("1 Minute Relaxing Maditation")
+            .font(.title)
+            .foregroundColor(.white)
+        
+    }
+    
+    //MARK: -
+    private var sliderWithTime: some View {
+        VStack(spacing: 5) {
+            Slider(value: $value, in: 0...60)
+                .accentColor(.white)
+            
+            HStack {
+                Text("0:00")
+                
+                Spacer()
+                
+                Text("1:00")
+            }
+            .font(.caption)
+            .foregroundColor(.white)
+        }
     }
 }
 

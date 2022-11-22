24549baa97d267b2a4d9b6f32a448902be5c1b62
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-11-22 16:36:24 +0200

    initail app project file

---
+++ MusicMeditation/MusicMeditation/MusicMeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicMeditationApp.swift
+//  MusicMeditation
+//
+//  Created by Ahmed Amin on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicMeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
